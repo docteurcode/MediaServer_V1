767265e92242d9ec8885d7f815b1a32627700947
Anwar Abir <abuanwar072@gmail.com>
2019-11-01 23:05:51 +0600

    Live Tv models created

--- liveTV/admin.py
+++ liveTV/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Catagory, Channel
 
 # Register your models here.
+
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name',  'popular', 'date')
+    list_filter = ('caragory',)
+    search_fields = ('name', 'overview', 'link_1', 'link_2')
+    list_per_page = 20
+
+
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date')
+    list_per_page = 20
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Catagory, CatagoryAdmin)
--- liveTV/models.py
+++ liveTV/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Catagory(models.Model):
+    name = models.CharField(max_length=250)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Channel (models.Model):
+    name = models.CharField(max_length=350)
+    caragory = models.ManyToManyField(Catagory)
+    overview = models.TextField(blank=True)
+    popular = models.BooleanField(default=False)
+    poster = models.ImageField(upload_to='media/liveTv/%Y/%m/%d/')
+    backdrop = models.ImageField(
+        upload_to='media/liveTv/%Y/%m/%d/', blank=True)
+    link_1 = models.CharField(max_length=500)
+    link_2 = models.CharField(max_length=500, blank=True)
+    date = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.name
